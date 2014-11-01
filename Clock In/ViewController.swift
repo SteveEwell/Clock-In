66cc76a6ef8fae1efcba84e505a8e8cbea7833f9
Steve Ewell <darksle@gmail.com>
2014-11-01 07:50:45 -0400

    Initial Commit

---
+++ Clock In/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Clock In
+//
+//  Created by Steve Ewell on 11/1/14.
+//  Copyright (c) 2014 Steve Ewell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
