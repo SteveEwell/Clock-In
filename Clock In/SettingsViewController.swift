de709c4647e64b3fc3ed6ac104db918c2584d7f3
Steve Ewell <darksle@gmail.com>
2014-11-25 11:06:15 -0500

    Working on settings controller, currently crashes.

--- Clock In/SettingsViewController.swift
+++ Clock In/SettingsViewController.swift
@@ -8,17 +8,14 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController, UITextFieldDelegate {
+class SettingsViewController: UITableViewController, UITextFieldDelegate {
     
-    @IBOutlet weak var employeeName:UITextField!
-    @IBOutlet weak var employeeNumber:UITextField!
-    @IBOutlet weak var label:UILabel!
-    var employeeNameValue:String!
-    
-    var firstResponder:UIView?
+    var name = "Steve"
+    var number = "478112"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationItem.rightBarButtonItem = editButtonItem()
     }
     
     override func didReceiveMemoryWarning() {
@@ -26,13 +23,43 @@ class SettingsViewController: UIViewController, UITextFieldDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    func textFieldShouldReturn(textField: UITextField) -> Bool {
-        return true
+    override func tableView(tableView: UITableView, editingStyleForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCellEditingStyle {
+        return .None
     }
     
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath:indexPath) as SettingsTableViewCell
+        switch indexPath.section {
+            case 0:
+                cell.textField.text = self.name
+            case 1:
+                cell.textField.text = self.number
+                cell.textField.keyboardType = .NumbersAndPunctuation
+            default: break
+        }
+        cell.textField.delegate = self
+        return cell
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.endEditing(true)
+        return false
+    }
     
     func textFieldDidEndEditing(textField: UITextField) {
-        label.text = employeeName.text
+        var view:UIView = textField
+        do {
+            view = view.superview!
+        }
+        while !(view is UITableViewCell)
+        
+        let cell = view as SettingsTableViewCell
+        
+        let indexPath = self.tableView.indexPathForCell(cell)!
+        if indexPath.section == 0 {
+            self.name = cell.textField.text
+        } else if indexPath.section == 1 {
+            self.number = cell.textField.text
+        }
     }
-    
 }
