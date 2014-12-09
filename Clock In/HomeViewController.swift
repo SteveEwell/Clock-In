1c48f926e43c8477f07fd3bb06fc3583c7211ec2
Steve Ewell <steve@ewell.io>
2014-12-09 11:11:00 -0500

    Fixed disclaimer popup. Added launch screens.

--- Clock In/HomeViewController.swift
+++ Clock In/HomeViewController.swift
@@ -15,21 +15,14 @@ class HomeViewController: UIViewController {
     
     let generator:CoreGenerator = CoreGenerator()
     let empNumberToUPC:EmpNumberToUPCA = EmpNumberToUPCA()
-    
+    var firstLaunch:Bool? = Bool()
     var employeeNameString:String? = String()
     var employeeNumberString:String? = String()
-    var firstLaunch:Bool? = Bool()
     var empNumber:String? = String()
     var barCode:UIImage? = UIImage()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        firstLaunch = NSUserDefaults.standardUserDefaults().boolForKey("firstLaunch")
-        if (firstLaunch == true) {
-            let alert = UIAlertController(title: "Disclamer!", message: "This app is not a replacement for your company provided clock-in/clock-out method. It is intended for convenience only. Please contact your companies HR representative for specific rules regarding this apps usage.", preferredStyle: .Alert)
-            alert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: alertHandler))
-            self.presentViewController(alert, animated: true, completion: nil)
-        }
     }
     
     override func viewWillAppear(animated:Bool) {
@@ -37,11 +30,20 @@ class HomeViewController: UIViewController {
         NSUserDefaults.standardUserDefaults().synchronize()
         employeeNameString = NSUserDefaults.standardUserDefaults().stringForKey("employeeName")
         employeeNumberString = NSUserDefaults.standardUserDefaults().stringForKey("employeeNumber")
-        firstLaunch = NSUserDefaults.standardUserDefaults().boolForKey("firstLaunch")
         empNumber = empNumberToUPC.convertEmpNum(employeeNumberString!)
         barCode = generator.generateCode(empNumber!)
         employeeNameLable.text = employeeNameString!.uppercaseString
         barCodeView.image = barCode
+    }
+    
+    override func viewDidAppear(animated:Bool) {
+        super.viewDidAppear(true)
+        firstLaunch = NSUserDefaults.standardUserDefaults().boolForKey("firstLaunch")
+        if (firstLaunch == true) {
+            let alert = UIAlertController(title: "Disclamer!", message: "This app is not a replacement for your company provided clock-in/clock-out method. It is intended for convenience only. Please contact your companies HR representative for specific rules regarding this apps usage.", preferredStyle: .Alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: alertHandler))
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -49,13 +51,13 @@ class HomeViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func unwind(seg:UIStoryboardSegue!) {
+        
+    }
+    
     func alertHandler(act:UIAlertAction!) {
         NSUserDefaults.standardUserDefaults().setValue(false, forKey: "firstLaunch")
         NSUserDefaults.standardUserDefaults().synchronize()
-    }
-    
-    @IBAction func unwind(seg:UIStoryboardSegue!) {
-        
     }
 
 
